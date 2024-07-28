ddaee02a4651d3f75c80425f02b00ad907842e16
Vladislav <M1LkMenIO@yandex.ru>
2024-07-28 20:10:28 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ public class Main {
         if (year > 1584) {
             if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
                 System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
             }
         } else {
             System.out.println(year + " год не является високосным");
         }
     }
+
 
     public static void applicationVersion(int clientOS, int clientDeviceYear) {
         if (clientDeviceYear < 2015) {
